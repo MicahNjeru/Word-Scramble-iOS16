dfdc46e991fd0141d33a2fffc489e6be01ba282e
Micah Njeru <micahnjeru1@gmail.com>
2023-05-17 11:59:07 +0300

    Initial local commit for Word Scramble iOS16

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Micah Jesse Njeru on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
